980a963ae31d5491b69fdc510d41900bcd423c54
bwogit <jeremie.sandot@hotmail.com>
2023-01-15 20:19:37 +0000

    Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,11 @@ document.addEventListener("DOMContentLoaded", function(){
         }
         })
     }
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key==="Enter"){
+            checkAnswer();
+        }
+    })
     runGame("addition")
 })
 
@@ -21,6 +26,9 @@ document.addEventListener("DOMContentLoaded", function(){
  * The main game "loop", called when the script is loaded and after the answer has been processed
  */
 function runGame (gameType) {
+
+    document.getElementById("answer-box").value="";
+    document.getElementById("answer-box").focus();
     //creates 2 ranndom numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
